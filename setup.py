2e718293036107652d060daa484410ae944cec14
Işık Kaplan <isik.kaplan@hotmail.com>
2019-03-28 02:04:39 +0300

    description for setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from setuptools import setup
 
+
+with open('README.md') as f:
+    long_description = f.read()
+
+
 setup(
     name='django_urls',
     version='1.0.0',
     packages=['django_urls'],
     url='https://github.com/isik-kaplan/django_urls',
+    description="URL decorator for django views",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license='APGL-3.0',
     author='isik-kaplan',
     author_email='',
-    description='URL decorators for django',
     install_requires=['django>=2.0']
 )
